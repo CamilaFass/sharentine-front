be8f0bfa4c4dacbdd98aea703798db75a3c873d1
Camila Fassarella <fassarella.camila@gmail.com>
2020-10-12 18:14:43 -0300

    homepage almost complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Menu from "./menu/Menu";
 import Footer from "./footer/Footer";
+import AboutUs from "./aboutus/AboutUs";
+import How from "./how/how";
+import TopPage from "./toppage/TopPage";
 
 function App() {
   return (
     <div className="App">
       <Menu />
+      <TopPage />
+      <How />
+      <AboutUs />
       <Footer />
     </div>
   );
