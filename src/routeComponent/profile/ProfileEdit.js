d063edbd3bd1d02c635c9acdf6ad01a13caaac1d
Victor Monteiro <victor_monteiro@live.com>
2020-10-15 01:44:10 -0300

    created route to profile-edit

--- src/routeComponent/profile/ProfileEdit.js
+++ src/routeComponent/profile/ProfileEdit.js
@@ -3,51 +3,63 @@ import React from 'react';
 function ProfileEdit(props) {
   return (
     <div className="col-md-9">
-<form class="needs-validation" novalidate>
-  <div class="form-row">
-    <div class="col-md-6 mb-3">
-      <label for="validationTooltip01">First name</label>
-      <input type="text" class="form-control" id="validationTooltip01" value="Mark" required>
-      <div class="valid-tooltip">
-        Looks good!
+      <div className="profile-content">
+        <form className="needs-validation" novalidate>
+          <div className="form-row">
+            <div className="col-md-6 mb-3">
+              <label htmlFor="validationTooltip03">Change Photo</label>
+              <input type="file" className="form-control" />
+              <button className="btn btn-success btn-sm mt-2">Upload</button>
+            </div>
+          </div>
+          <div className="form-row">
+            <div className="col-md-6 mb-3">
+              <label for="validationTooltip01">First name</label>
+              <input
+                type="text"
+                className="form-control"
+                id="validationTooltip01"
+                // value={state.user.name}
+                required
+              />
+              <div className="valid-tooltip">Looks good!</div>
+            </div>
+            <div className="col-md-6 mb-3">
+              <label for="validationTooltip02">Last name</label>
+              <input
+                type="text"
+                className="form-control"
+                id="validationTooltip02"
+                // value={state.user.lastName}
+                required
+              />
+              <div className="valid-tooltip">Looks good!</div>
+            </div>
+          </div>
+          <div className="form-row">
+            <div className="col-md-6 mb-3">
+              <label htmlFor="validationTooltip03">City</label>
+              <input
+                type="text"
+                className="form-control"
+                id="validationTooltip03"
+                required
+              />
+              <div className="invalid-tooltip">
+                Please provide a valid city.
+              </div>
+            </div>
+          </div>
+          <div className="d-flex justify-content-around">
+            <button className="btn btn-success btn-sm" type="submit">
+              Save Changes
+            </button>
+            <button className="btn btn-danger btn-sm" type="submit">
+              Delete Account
+            </button>
+          </div>
+        </form>
       </div>
-    </div>
-    <div class="col-md-6 mb-3">
-      <label for="validationTooltip02">Last name</label>
-      <input type="text" class="form-control" id="validationTooltip02" value="Otto" required>
-      <div class="valid-tooltip">
-        Looks good!
-      </div>
-    </div>
-  </div>
-  <div class="form-row">
-    <div class="col-md-6 mb-3">
-      <label for="validationTooltip03">City</label>
-      <input type="text" class="form-control" id="validationTooltip03" required>
-      <div class="invalid-tooltip">
-        Please provide a valid city.
-      </div>
-    </div>
-    <div class="col-md-3 mb-3">
-      <label for="validationTooltip04">State</label>
-      <select class="custom-select" id="validationTooltip04" required>
-        <option selected disabled value="">Choose...</option>
-        <option>...</option>
-      </select>
-      <div class="invalid-tooltip">
-        Please select a valid state.
-      </div>
-    </div>
-    <div class="col-md-3 mb-3">
-      <label for="validationTooltip05">Zip</label>
-      <input type="text" class="form-control" id="validationTooltip05" required>
-      <div class="invalid-tooltip">
-        Please provide a valid zip.
-      </div>
-    </div>
-  </div>
-  <button class="btn btn-primary" type="submit">Submit form</button>
-</form>
     </div>
   );
 }
