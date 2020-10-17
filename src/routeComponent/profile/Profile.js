665df4520bdc482923d0ab839d29600620d5717f
Victor Monteiro <victor_monteiro@live.com>
2020-10-17 01:02:46 -0300

    profile done

--- src/routeComponent/profile/Profile.js
+++ src/routeComponent/profile/Profile.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import ProfileCard from '../../components/profileCard/ProfileCard';
 import api from '../../apis/';
+import PostCard from '../../components/postCard/PostCard';
 
 function Profile(props) {
   const [state, setState] = useState({
@@ -10,28 +11,44 @@ function Profile(props) {
     error: ''
   });
 
-  useEffect(
-    function () {
-      setState({ ...state, loading: true });
+  useEffect(function () {
+    setState({ ...state, loading: true });
 
-      (async function () {
-        try {
-          const response = await api.get(`/post/${props.user._id}`);
-          console.log('TESTANDO', response);
+    (async function () {
+      // console.log('TESTANDO POST', state);
+      try {
+        const response = await api.get(`/post/${props.user._id}`);
 
-          // setState({ ...state, loading: false, posts: [...response.data] });
-        } catch (err) {
-          console.error(err);
-          setState({ ...state, loading: false, error: err.message });
-        }
-      })();
-    },
-    [props]
-  );
+        setState({ ...state, loading: false, posts: [...response.data.posts] });
+      } catch (err) {
+        console.error(err);
+        setState({ ...state, loading: false, error: err.message });
+      }
+    })();
+  }, []);
 
   return (
-    <div>
+    <div className="d-flex">
       <ProfileCard {...props} />
+
+      <div className="d-flex col-md-9 flex-column align-items-center">
+        {state.posts.map((item) => {
+          console.log('TESTANDO', item.userId);
+          return (
+            <PostCard
+              userImage={props.user.image}
+              name={props.user.name}
+              image={item.image}
+              title={item.title}
+              content={item.content}
+              key={item._id}
+              postUserId={item.userId}
+              activeUserId={props.user._id}
+              postId={item._id}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
